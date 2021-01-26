5c5f93c360f6824c334e32a51b7042ea9653052f
Mohamed Ramdan <64365635+MoRamdan11@users.noreply.github.com>
2021-01-26 17:51:38 +0200

    Create loss.py

---
+++ loss/loss.py
@@ -0,0 +1,10 @@
+class Loss:
+    def __init__(self):
+        self.type = 'normalization'
+        self.Y = 0
+
+    def forward(self, y_hat, y):
+        pass
+
+    def backward(self, y_hat):
+        pass
