01d6972d964596c9c80824f9e9d716fedf28c547
Mohamed Ramdan <64365635+MoRamdan11@users.noreply.github.com>
2021-01-26 17:50:17 +0200

    Create activation.py

---
+++ activation/activation.py
@@ -0,0 +1,10 @@
+class Activation:
+    def __init__(self):
+        self.type = 'activation'
+        self.Z = 0
+
+    def forward(self, z):
+        pass
+
+    def backward(self, grad):
+        pass
